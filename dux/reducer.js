457cb00889bd9157e397c0a9416969f86e44df8b
Tiffany Zhong <tiffzhong@gmail.com>
2019-02-28 22:20:27 -0800

    reducer

--- dux/reducer.js
+++ dux/reducer.js
@@ -1,23 +1,23 @@
 const INITIAL_STATE = {
-   access_code: ""
+  access_code: ""
 };
 
 const GET_ACCESS = "GET_ACCESS";
 
 export default function reducer(state = INITIAL_STATE, action) {
-   console.log("reducerlog", action.type, action.payload);
-   switch (action.type) {
-      case GET_ACCESS:
-         return { ...state, access_code: action.payload };
-      default:
-         return state;
-   }
+  console.log("reducerlog", action.type, action.payload);
+  switch (action.type) {
+    case GET_ACCESS:
+      return { ...state, access_code: action.payload };
+    default:
+      return state;
+  }
 }
 
 export function setAccessCode(code) {
-   console.log("this is running", code);
-   return {
-      type: GET_ACCESS,
-      payload: code
-   };
+  console.log(code);
+  return {
+    type: GET_ACCESS,
+    payload: code
+  };
 }
